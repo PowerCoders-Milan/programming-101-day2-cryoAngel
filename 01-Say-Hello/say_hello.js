4a8e4a0fac7673e89fef2e4c5b4fdb3140258b02
Grace Kabeya <antoinefaith1@gmail.com>
2021-02-19 14:00:39 +0100

    exercise 1 and 2

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,18 @@
 //insert your pseudocode below
 /*
+set an enter prompt with my name
+the print a greeting message with the name i setted
 
-here
+
+first create a variable with your name
+set the programm to prompt a greeting text 
 
 */
 
 //insert your code below
+var greeting;
+
+
+greeting = 'welcome grace';
+window.alert(window.prompt('type your name'));
+window.alert(greeting);
--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -2,7 +2,13 @@
 /*
 
 here
-
+set a variable with his value
+then set a function that will count the characters of the value 
 */
 
 //insert your code below
+var string;
+
+
+string = 'abcd';
+window.alert(string.length);
