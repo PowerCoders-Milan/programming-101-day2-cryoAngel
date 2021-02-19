02c6482dbeef77c5ad020831e37393901dd91bb9
Grace Kabeya <antoinefaith1@gmail.com>
2021-02-19 14:39:30 +0100

    exercise 3

--- 03-Find-My-Number/find_my_number.js
+++ 03-Find-My-Number/find_my_number.js
@@ -1,8 +1,23 @@
 //insert your pseudocode below
 /*
 
-here
+setting a list of numbers inside a variable
+then set a function that return me numbers randomly
+
 
 */
 
 //insert your code below
+var myNumber;
+
+function listsGetRandomItem(list, remove) {
+  var x = Math.floor(Math.random() * list.length);
+  if (remove) {
+    return list.splice(x, 1)[0];
+  } else {
+    return list[x];
+  }
+}
+
+myNumber = [50, 28, 3, 7];
+window.alert(listsGetRandomItem(myNumber, false));
